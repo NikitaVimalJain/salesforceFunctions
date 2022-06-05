f8405eac9aa9b75877810f8afe6a83f3e35db9a1
Nikita Jain <nvimalkumarjain@deloitte.com>
2022-06-06 02:32:39 +0530

    commit

--- functions/myfunction/index.js
+++ functions/myfunction/index.js
@@ -1,22 +1,20 @@
 /**
  * Describe Myfunction here.
  *
- * The exported method is the entry point for your code when the function is invoked. 
+ * The exported method is the entry point for your code when the function is invoked.
  *
- * Following parameters are pre-configured and provided to your function on execution: 
- * @param event: represents the data associated with the occurrence of an event, and  
+ * Following parameters are pre-configured and provided to your function on execution:
+ * @param event: represents the data associated with the occurrence of an event, and
  *                 supporting metadata about the source of that occurrence.
  * @param context: represents the connection to Functions and your Salesforce org.
  * @param logger: logging handler used to capture application logs and trace specifically
  *                 to a given execution of a function.
  */
- 
-export default async function (event, context, logger) {
-    logger.info(`Invoking Myfunction with payload ${JSON.stringify(event.data || {})}`);
 
-    const results = await context.org.dataApi.query('SELECT Id, Name FROM Account');
+export default async function execute(event, context, logger) {
+  const query = "SELECT Id, Name FROM Account";
+  const results = await context.org.dataApi.query(query);
+  logger.info(JSON.stringify(results));
 
-    logger.info(JSON.stringify(results));
-
-    return results;
+  return results;
 }
